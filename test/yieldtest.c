4c92faa9a63f27888893821269b401499a75007e
Yuan Fu <fuyuanx@gmail.com>
2015-10-31 16:28:01 -0700

    delete all

--- test/yieldtest.c
+++
@@ -1,8 +0,0 @@
-#include "syscall.h"
-
-int main(){
-        Write("\nTesting Yield\n",sizeof("\nTesting Yield\n"),ConsoleOutput);
-        Yield();
-        Write("\nTesting Yield successfully\n",sizeof("\nTesting Yield successfully\n"),ConsoleOutput);
-
-}
